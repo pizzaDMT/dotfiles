67c77694f30688065d6904ceeb11e95a2676e64c
wutnever <160237229+wutnvr@users.noreply.github.com>
2025-05-25 21:32:27 -0300

    Update Microsoft.PowerShell_profile.ps1

--- themes/Microsoft.PowerShell_profile.ps1
+++ themes/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,2 @@
-oh-my-posh.exe init pwsh --config "C:\Users\REPLACE\mint.omp.json" | Invoke-Expression
+oh-my-posh.exe init pwsh --config "C:\Users\REPLACE\wutnvr_forest.omp.json" | Invoke-Expression
 fastfetch
